1b51b9f655c00051eda58281379890dcd7ba6ab2
jooda00 <68958749+jooda00@users.noreply.github.com>
2023-11-19 22:36:23 +0900

    [폰켓몬]

---
+++ 주다애/Hash/prob2.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int size = nums.length / 2;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(Integer num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        if (map.size()  >= size) {
+            answer = size;
+            return answer;
+        }
+        answer = map.size();
+        return answer;
+    }
+}
