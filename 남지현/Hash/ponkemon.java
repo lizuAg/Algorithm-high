59899047bd41996e104a9ee44fb90d471d709695
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2023-11-19 18:27:23 +0900

    [Refactoring] 해시 - 폰켓몬

--- 남지현/Hash/ponkemon.java
+++ 남지현/Hash/ponkemon.java
@@ -2,17 +2,11 @@ import java.util.*;
 import java.lang.Math;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         int numberOfMine = nums.length/2;
         Map<Integer, Integer> collection = new HashMap<>();
-        for (int ponkemon: nums) {
-            if (collection.containsKey(ponkemon))
-                collection.put(ponkemon, collection.get(ponkemon)+1);
-            else
-                collection.put(ponkemon, 1);
-        }
-        int numberOfTypes = collection.keySet().size();
-        answer = Math.min(numberOfTypes, numberOfMine);
+        for (Integer ponkemon : nums)
+            collection.put(ponkemon, collection.getOrDefault(ponkemon, 0) + 1);
+        int answer = Math.min(collection.keySet().size(), numberOfMine);
         return answer;
     }
 }
