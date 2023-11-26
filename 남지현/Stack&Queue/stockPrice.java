05220c53f99e48cf662d2c1345114c98829c0d3e
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2023-11-26 23:41:01 +0900

    [Problem Solving] 스택/큐 - 주식가격
    스택 사용 풀이 - 검색결과 참고

--- 남지현/Stack&Queue/stockPrice.java
+++ 남지현/Stack&Queue/stockPrice.java
@@ -2,13 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for(int i = 0; i < answer.length; i++) {
-            for(int j = i + 1; j < answer.length; j++) {
-                answer[i]++;
-                if(prices[i] > prices[j])
-                    break;
+        Stack<Integer> stack = new Stack<>();
+        int len = prices.length;
+        int[] answer = new int[len];
+        for (int i=0; i<len; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i-stack.peek();
+                stack.pop();
             }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length-stack.peek()-1;
+            stack.pop();
         }
         return answer;
     }
