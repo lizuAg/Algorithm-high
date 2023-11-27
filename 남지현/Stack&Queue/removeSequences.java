1e473d62d09979cb71dd742d12d7ff8e924f70c7
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2023-11-27 14:51:30 +0900

    [Refactoring] 스택/큐 - 같은 숫자는 싫어
    자바 Stack의 peek 메서드 사용

--- 남지현/Stack&Queue/removeSequences.java
+++ 남지현/Stack&Queue/removeSequences.java
@@ -3,15 +3,12 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         List<Integer> answer = new ArrayList<>();
-        
+
         Stack<Integer> stack = new Stack<>();
         for (int num: arr) {
             if (stack.empty()) {
                 stack.push(num);
-            } else {
-                int status = stack.pop();
-                if (status != num) 
-                    stack.push(status);
+            } else if (stack.peek() != num){
                 stack.push(num);
             }
         }
