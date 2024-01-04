2958e0844148e165038d95ee595aba5cc14be4df
lizuAg <wndms058@gmail.com>
2024-01-04 23:28:06 +0900

    add prob2 : 네트워크

---
+++ 이주은/DFS-BFS/network.java
@@ -0,0 +1,27 @@
+//[프로그래머스] 네트워크(https://school.programmers.co.kr/learn/courses/30/lessons/43162)
+
+class Solution {
+    int[] visited;
+    int answer;
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new int[n];
+
+        for(int i=0; i<n; i++)
+            if(visited[i] == 0){
+                dfs(computers, visited, i);
+                answer++;
+            }
+
+        return answer;
+    }
+    
+    public void dfs(int[][] computers, int[] visited, int now){
+        visited[now] = 1;
+        
+        for(int i=0; i<computers.length; i++){
+            if(visited[i]==0 && computers[now][i] == 1)
+                dfs(computers, visited, i);
+        }
+    }
+}
