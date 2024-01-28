360c4b7665c7045f76fecde78094016b63da1ba1
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2024-01-29 06:32:50 +0900

    [Update] 이분탐색 - 입국심사 비교조건 수정

--- 남지현/BinarySearch/immigration.java
+++ 남지현/BinarySearch/immigration.java
@@ -2,10 +2,11 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
+        long answer = 0;
         long left = 0;
         long right = (long) times[0] * n;
         long mid = 0;
-        while (left < right) {
+        while (left <= right) {
             mid=(left+right)/2;
             long sum=0;
             for (int time: times) {
@@ -14,9 +15,10 @@ class Solution {
             if (sum < n) {
                 left = mid+1;
             } else {
-                right = mid;
+                right = mid-1;
+                answer = mid;
             }
         }
-        return left;
+        return answer;
     }
 }
