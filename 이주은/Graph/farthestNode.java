9c14e2d1b8782733ff78cf2783ebe35d69ff8c4c
lizuAg <wndms058@gmail.com>
2024-02-04 17:51:24 +0900

    update prob1 : 가장 먼 노드 다익스트라로 풀면 안되고 BFS로 풀어야 했어요. TS#9 말고는 통과했어서 조금 아쉬울 지도 ~

--- 이주은/Graph/farthestNode.java
+++ 이주은/Graph/farthestNode.java
@@ -4,47 +4,39 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        int answer = 0, max = 0;
-        int[] costs = new int[n + 1];
-        boolean[] visited = new boolean[n + 1];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> arr1[1] != arr2[1] ? arr1[1] - arr2[1] : arr1[0] - arr2[0]);
-
-        for (int i = 2; i <= n; i++)
-            costs[i] = Integer.MAX_VALUE;
-
-        //[#node, cost]
-        int[] start = {1, 0};
-        pq.add(start);
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-
-            if (visited[curr[0]])
-                continue;
-
-            for (int[] vertex : edge) {
-                int[] to = new int[2];
-                if (!visited[vertex[1]] && vertex[0] == curr[0]) {
-                    to[0] = vertex[1];
-                } else if (!visited[vertex[0]] && vertex[1] == curr[0]) {
-                    to[0] = vertex[0];
-                } else
-                    continue;
-
-                to[1] = curr[1] + 1;
-                if (to[1] < costs[to[0]]) {
-                    costs[to[0]] = to[1];
-                    pq.add(to);
-                    
-                    if (to[1] == max) {
-                        answer++;
-                    } else if (to[1] > max) {
-                        answer = 1;
-                        max = to[1];
-                    }
+        int max = 0, answer = 0;
+        int[] costs = new int[n+1];
+        boolean[] visited = new boolean[n+1];
+        
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            
+            for(int[] e : edge){
+                int next;
+                if(!visited[e[1]] && curr == e[0]){
+                    next = e[1];
                 }
+                else if(!visited[e[0]] && curr == e[1]){
+                    next = e[0];
+                }
+                else
+                    continue;
+                
+                costs[next] = costs[curr] + 1;
+                visited[next] = true;
+                queue.add(next);
+                
+                if(costs[next] > max){
+                    max = costs[next];
+                    answer = 1;
+                }else if(costs[next] == max){
+                    answer ++;
+                }
+
             }
-            visited[curr[0]] = true;
         }
         return answer;
     }
