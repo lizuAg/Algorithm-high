b15401bf9fda22628a1c0e118de3b73fbbb76b09
lizuAg <wndms058@gmail.com>
2024-03-31 22:40:23 +0900

    refactor: 프로그래머스 주식가격

--- 이주은/Stack-Queue/stock.java
+++ 이주은/Stack-Queue/stock.java
@@ -1,22 +1,28 @@
-//[프로그래머스] 주식가격 (https://school.programmers.co.kr/learn/courses/30/lessons/42584)
+//[프로그래머스] 주식가격(https://school.programmers.co.kr/learn/courses/30/lessons/42584)
+
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
         int N = prices.length;
         int[] answer = new int[N];
+        Deque<Integer> stack = new ArrayDeque<>();
         
-        for(int i=0; i<N; i++){
+        for(int i=0; i<N; i++) {
             int curr = prices[i];
-            int days = 0;
-            for(int j=i+1; j<N; j++) {
-                days++;
-                int next = prices[j];
-                if(curr > next)
-                    break;
+            while(!stack.isEmpty() && prices[stack.peekFirst()] > curr) {
+                int idx = stack.removeFirst();
+                
+                answer[idx] = i-idx;
             }
-            answer[i] = days;
+            stack.addFirst(i);
         }
-
+        
+        while(!stack.isEmpty()){
+            int idx = stack.removeFirst();
+            answer[idx] = N-idx-1;
+        }
         return answer;
     }
 }
+
