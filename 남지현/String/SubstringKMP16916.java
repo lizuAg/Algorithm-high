c2702b6790c469ee0a34400ee7cf0a280d57386f
NamJihyun99 <87467801+NamJihyun99@users.noreply.github.com>
2024-06-10 18:56:31 +0900

    [Problem Solving] 백준16916 브론즈2 - 부분문자열

--- 남지현/String/SubstringKMP16916.java
+++ 남지현/String/SubstringKMP16916.java
@@ -12,19 +12,19 @@ class Main {
         int N = str.length;
         int n = sub.length;
         int j = 0;
-        int[] lps = new int[n];
+        int[] pi = new int[n];
         for (int i=1; i<n; i++) {
             while (j>0 && sub[i]!=sub[j]) {
-                j = lps[j-1];
+                j = pi[j-1];
             }
             if (sub[i]==sub[j]) {
-                lps[i] = ++j;
+                pi[i] = ++j;
             }
         }
         j = 0;
         for (int i=0; i<N; i++) {
             while (j>0 && str[i]!=sub[j]) {
-                j = lps[j-1];
+                j = pi[j-1];
             }
             if (str[i]==sub[j]) {
                 if (j==n-1) {
