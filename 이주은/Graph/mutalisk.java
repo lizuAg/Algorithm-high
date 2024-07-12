4329fc84388036e30ffd65ad89c859b35bde7725
이주은 <68546023+lizuAg@users.noreply.github.com>
2024-07-12 23:42:50 +0900

    Update mutalisk.java

--- 이주은/Graph/mutalisk.java
+++ 이주은/Graph/mutalisk.java
@@ -3,45 +3,34 @@ import java.io.*;
 
 class Main {
     static int N;
-    static int[] arr;
-    static int[][][] dp;
+    static int[] scv;
+    static int[][][] memo;
 
-    static int[] attack = {9, 3, 1};
-    
+    static int[][] attack = {{9, 3, 1}, {9, 1, 3}, 
+                               {3, 9, 1}, {3, 1, 9}, 
+                               {1, 9, 3}, {1, 3, 9}};
+
     public static void main(String[] args) throws IOException {
         getInput();
+        
+        dfs(scv[0], scv[1], scv[2], 0);
 
-        dfs(arr[0], arr[1], arr[2], 0);
-
-        System.out.println(dp[0][0][0]);   
+        System.out.println(memo[0][0][0]);   
     }
 
     private static void dfs(int a, int b, int c, int depth) {
-        a = Math.max(a, 0);
-        b = Math.max(b, 0);
-        c = Math.max(c, 0);
-        
-        if(dp[a][b][c] == 0)
-            dp[a][b][c] = depth;
-        else if(dp[a][b][c] < depth)
+        if(memo[0][0][0] <= depth)
+            return;
+        if(memo[a][b][c] != 0 && memo[a][b][c] <= depth)
             return;
         else
-            dp[a][b][c] = Math.min(dp[a][b][c], depth);
-        
+            memo[a][b][c] = depth;
+
         if(a == 0 && b == 0 && c == 0)
             return;
-        
-        for(int i=0; i<3; i++) {
-            for(int j=0; j<3; j++) {
-                if(i==j)
-                    continue;
-                for(int k=0; k<3; k++) {
-                    if(i == k || j == k)
-                        continue;
-                    
-                    dfs(a-attack[i], b-attack[j], c-attack[k], depth+1);
-                }
-            }
+
+        for(int i=0; i<6; i++) {
+            dfs(Math.max(a-attack[i][0], 0), Math.max(b-attack[i][1], 0), Math.max(c-attack[i][2], 0), depth+1);
         }
     }
 
@@ -49,12 +38,13 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[3];
+        scv = new int[3];
         String[] line = br.readLine().split(" ");
         for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(line[i]);
+            scv[i] = Integer.parseInt(line[i]);
         }
-        
-        dp = new int[arr[0]+1][arr[1]+1][arr[2]+1];
+
+        memo = new int[scv[0]+1][scv[1]+1][scv[2]+1];
+        memo[0][0][0] = 61;
     }
 }
